02ef3d8a7d31e874abf3848119c43addbefa714b
Tomas <tomdtron@gmail.com>
2024-07-03 21:25:56 +0200

    update refinement naming

--- backend/Refinement.v
+++ backend/Refinement.v
@@ -40,18 +40,18 @@ Qed.
 (***********************************)
 (*          Monotonous             *)
 (***********************************)
-Definition is_monotonous {A B} `{Refinable A} `{Refinable B} (f : A -> B) := 
+Definition is_monotone {A B} `{Refinable A} `{Refinable B} (f : A -> B) := 
     forall a1 a2, a1 ⊑ a2 -> f a1 ⊑ f a2.
 
-Lemma fun_is_monotonous : forall {A B} `{Refinable A} `{Refinable B},
-    forall a, is_monotonous (fun f : A -> B => f a).
+Lemma fun_is_monotone : forall {A B} `{Refinable A} `{Refinable B},
+    forall a, is_monotone (fun f : A -> B => f a).
 Proof.
   red; simpl; intros; eauto.
 Qed.
 
-Existing Class is_monotonous.
+Existing Class is_monotone.
 
-#[export] Hint Resolve fun_is_monotonous : typeclass_instances.
+#[export] Hint Resolve fun_is_monotone : typeclass_instances.
     
 (***********************************)
 (*          Complete               *)
@@ -130,46 +130,57 @@ exact I
 
 
 #[export]
-Instance eqGroundTrue (A : Type) (refEqA := eqRefinable A) (compA := eqCompleteTrue A) : Ground A.
+Instance eqGroundTrue (A : Type) (refEqA := eqRefinable A) (compA := eqCompleteTrue A) : Ground A | 100.
 Proof. econstructor. eauto. Qed.
 
   
 
-Section Monotonicity.
+Section IncReformulation.
   Context {A} {HA: Refinable A} {HAC : Complete A}.
 
-  Class Monotonizable (P : A -> Prop) :=
+  Class IncRef (P : A -> Prop) :=
     {
-      monotone : A -> Prop;
-      antitone : A -> Prop;
-      is_monotone : forall a1 a2, a1 ⊑ a2 -> monotone a1 -> monotone a2;
-      is_antitone : forall a1 a2, a1 ⊑ a2 -> antitone a2 -> antitone a1;
-      complete_monotone_is_equivalent : forall (a : A), is_complete a -> monotone a <-> P a;
-      complete_antitone_is_equivalent : forall (a : A), is_complete a -> antitone a <-> P a
-    }.
+      ir_mono : A -> Prop;
+      ir_anti : A -> Prop;
+      is_monotone_mono : forall a1 a2, a1 ⊑ a2 -> ir_mono a1 -> ir_mono a2;
+      is_antitone_anti : forall a1 a2, a1 ⊑ a2 -> ir_anti a2 -> ir_anti a1;
+      approx_mono : forall a, P a -> ir_mono a;
+      approx_anti : forall a, ir_anti a -> P a;
+      recoverability_mono : forall (a : A), is_complete a -> ir_mono a -> P a;
+      recoverability_anti : forall (a : A), is_complete a -> P a -> ir_anti a
+      }.
 
-  Arguments monotone P {Monotonizable} a. 
-  Arguments antitone P {Monotonizable} a. 
+  Arguments ir_mono P {IncRef} a. 
+  Arguments ir_anti P {IncRef} a. 
 
+  Lemma complete_monotone_is_equivalent {P} `{IncRef P} : forall (a : A), is_complete a -> ir_mono P a <-> P a.
+  Proof.
+    intros; split; [ apply recoverability_mono; eauto | apply approx_mono].
+  Qed.
+
+  Lemma complete_antitone_is_equivalent {P} `{IncRef P} : forall (a : A), is_complete a -> ir_anti P a <-> P a.
+  Proof.
+    intros; split; [ apply approx_anti | apply recoverability_anti; eauto].
+  Qed.
 
   Obligation Tactic :=  try now eauto.
 
   #[export] 
-  Program Instance monotonizableConst (P : Prop) : Monotonizable (fun a => P) := {
-      monotone := fun _ => P ;
-      antitone := fun _ => P ;
+  Program Instance IncRefConst (P : Prop) : IncRef (fun a => P) := {
+      ir_mono := fun _ => P ;
+      ir_anti := fun _ => P ;
     }.
   
   Obligation Tactic :=  try now intuition.
 
   #[export] 
-  Program Instance monotonizableEq {B} `{HB : Ground B}
-    (g h : A -> B) (Hcg : is_monotonous g) (Hch : is_monotonous h)
+  Program Instance IncRefEq {B} `{HB : Ground B}
+    (g h : A -> B) (Hcg : is_monotone g) (Hch : is_monotone h)
     (Hcg' : is_complete g) (Hch' : is_complete h)
-    : Monotonizable (fun a => g a = h a) | 2
+    : IncRef (fun a => g a = h a) | 2
     := {
-      monotone := fun a => exists b, b ⊑ g a /\ b ⊑ h a ;
-      antitone := fun a => is_complete (g a) /\ g a = h a ; 
+      ir_mono := fun a => exists b, b ⊑ g a /\ b ⊑ h a ;
+      ir_anti := fun a => is_complete (g a) /\ g a = h a ; 
     }.
   Next Obligation.
     intros ? ? ? ? g h ? ? ? ? a1 a2 Hprec [b [? ?]].
@@ -185,20 +196,23 @@ Section Monotonicity.
     - eauto.
   Qed.
   Next Obligation.
-    intros B ? ? ? g h ? ? Hcg Hch a Hca. split.
-    - intros [b [Hb1 Hb2]].
-      eapply is_complete_minimal in Hb1; eauto.
-      eapply is_complete_minimal in Hb2; eauto; subst.
-      assumption.
-    - intros ->. eexists; split; reflexivity.
+  intros B ? ? ? g h ? ? Hcg Hch a Hca.
+  cbn in *. eexists. split; try reflexivity. rewrite Hca; reflexivity.  
+  Qed. 
+  Next Obligation.
+    intros B ? ? ? g h ? ? Hcg Hch a Hca.
+    intros [b [Hb1 Hb2]].
+    eapply is_complete_minimal in Hb1; eauto.
+    eapply is_complete_minimal in Hb2; eauto; subst.
+    assumption.
   Qed.
 
   #[export] 
-  Program Instance monotonizableEqL {B} `{HB : Ground B}
-    (g : A -> B) (Hcg : is_complete g) (Hmonog : is_monotonous g) 
-    (b : B) : Monotonizable (fun a => g a = b) | 1 := {
-      monotone := fun a => b ⊑ g a ;
-      antitone := fun a => is_complete (g a) /\ g a = b ;
+  Program Instance IncRefEqL {B} `{HB : Ground B}
+    (g : A -> B) (Hcg : is_complete g) (Hmonog : is_monotone g) 
+    (b : B) : IncRef (fun a => g a = b) | 1 := {
+      ir_mono := fun a => b ⊑ g a ;
+      ir_anti := fun a => is_complete (g a) /\ g a = b ;
     }.
   Next Obligation.
     cbn; intros ? ? ? ? g Hcg ? b a1 a2 Hprec ?.
@@ -210,180 +224,216 @@ Section Monotonicity.
     rewrite Hmono; eauto.
   Qed.
   Next Obligation.
-    intros B ? ? ? g ? ? ? a Hca. split; [intros Hbg | intros <-].
-    - eapply is_complete_minimal in Hbg; eauto.
-    - reflexivity.
+    intros B ? ? ? g ? ? ? a <-. reflexivity.
+  Qed.
+  Next Obligation.
+    intros B ? ? ? g ? ? ? a Hca. intros Hbg.
+    cbn; eapply is_complete_minimal in Hbg; eauto.
   Qed.
 
   #[export] 
-  Program Instance monotonizableForall {B} {P : B -> A -> Prop} `{HPB : forall b, Monotonizable (P b)} :      
-    Monotonizable (fun a => forall b, P b a) :=
+  Program Instance IncRefForall {B} {P : B -> A -> Prop} `{HPB : forall b, IncRef (P b)} :      
+    IncRef (fun a => forall b, P b a) :=
     {
-      monotone := fun a => forall b, monotone (P b) a ;
-      antitone := fun a => forall b, antitone (P b) a ;
+      ir_mono := fun a => forall b, ir_mono (P b) a ;
+      ir_anti := fun a => forall b, ir_anti (P b) a ;
     }.
   Next Obligation.
     intros B P HPB a1 a2 Hprec Hmono; simpl; intros b.
-    eapply is_monotone.
+    eapply is_monotone_mono.
     - apply Hprec.
     - eauto.
   Qed.
   Next Obligation.
     intros B P HPB a1 a2 Hprec Hmono; simpl; intros b.
-    eapply is_antitone.
+    eapply is_antitone_anti.
     - apply Hprec.
     - apply Hmono.
   Qed.
   Next Obligation with eauto.
-    intros B P HPB a Hac; simpl; split; intros HP b; eapply (HPB b).(complete_monotone_is_equivalent)...
+    intros B P HPB a Hac; simpl in *. intros b; eapply (HPB b).(approx_mono)...
   Qed.
   Next Obligation with eauto.
-    intros B P HPB a Hac; simpl; split; intros HP b; eapply (HPB b).(complete_antitone_is_equivalent)...
+    intros B P HPB a Hac; simpl in *. intros b; eapply (HPB b).(approx_anti)...
+  Qed.
+  Next Obligation with eauto.
+    intros B P HPB a Hac; simpl; intros HP b; eapply complete_monotone_is_equivalent...
+  Qed.
+  Next Obligation with eauto.
+    intros B P HPB a Hac; simpl; intros HP b; eapply complete_antitone_is_equivalent...
   Qed.
 
   #[export] 
-  Program Instance monotonizableExists {B} `{HB: Refinable B} 
-    {P : B -> A -> Prop} `{HPB : forall b, Monotonizable (P b)} 
-    : Monotonizable (fun a => exists b, P b a) :=
+  Program Instance IncRefExists {B} `{HB: Refinable B} 
+    {P : B -> A -> Prop} `{HPB : forall b, IncRef (P b)} 
+    : IncRef (fun a => exists b, P b a) :=
     {
-      monotone := fun a => exists b, monotone (P b) a ;
-      antitone := fun a => exists b, antitone (P b) a ;
+      ir_mono := fun a => exists b, ir_mono (P b) a ;
+      ir_anti := fun a => exists b, ir_anti (P b) a ;
     }.
   Next Obligation.
     intros B HB P HPB a1 a2 Hprec [b HP]; simpl.
-    exists b; eapply is_monotone; try apply Hprec; eauto.
+    exists b; eapply is_monotone_mono; try apply Hprec; eauto.
   Qed.
   Next Obligation.
     intros B HB P HPB a1 a2 Hprec [b HP]; simpl.
-    exists b; eapply is_antitone; try apply Hprec; eauto.
+    exists b; eapply is_antitone_anti; try apply Hprec; eauto.
   Qed.
   Next Obligation with eauto.
-    intros B HB P HPB a Hac; simpl; split; intros [b HP]; exists b;
-      eapply (HPB b).(complete_monotone_is_equivalent)...
+    intros B HB P HPB a ; simpl in *; intros [b HP]; exists b;
+      eapply (HPB b).(approx_mono)...
   Qed.
   Next Obligation with eauto.
-    intros B HB P HPB a Hac; simpl; split; intros [b HP]; exists b;
-      eapply (HPB b).(complete_antitone_is_equivalent)...
+    intros B HB P HPB a ; simpl in *; intros [b HP]; exists b;
+      eapply (HPB b).(approx_anti)...
+  Qed.
+  Next Obligation with eauto.
+    intros B HB P HPB a Hac; simpl; intros [b HP]; exists b;
+      eapply complete_monotone_is_equivalent...
+  Qed.
+  Next Obligation with eauto.
+    intros B HB P HPB a Hac; simpl; intros [b HP]; exists b;
+      eapply complete_antitone_is_equivalent...
   Qed.
 
   #[export] 
-  Program Instance monotonizableConj (P Q : A -> Prop) {HP : Monotonizable P} {HQ : Monotonizable Q} :  
-    Monotonizable (fun a => P a /\ Q a) :=
+  Program Instance IncRefConj (P Q : A -> Prop) {HP : IncRef P} {HQ : IncRef Q} :  
+    IncRef (fun a => P a /\ Q a) :=
     {
-      monotone := fun a => (monotone P a) /\ (monotone Q a) ;
-      antitone := fun a => (antitone P a) /\ (antitone Q a) ;
+      ir_mono := fun a => (ir_mono P a) /\ (ir_mono Q a) ;
+      ir_anti := fun a => (ir_anti P a) /\ (ir_anti Q a) ;
     }.
   Next Obligation.
     intros P Q HP HQ a1 a2 Hprec [HP1 HQ1]; simpl; split.
-    - eapply is_monotone; try apply Hprec; eauto.
-    - eapply is_monotone; try apply Hprec; eauto.
+    - eapply is_monotone_mono; try apply Hprec; eauto.
+    - eapply is_monotone_mono; try apply Hprec; eauto.
   Qed.
   Next Obligation.
     intros P Q HP HQ a1 a2 Hprec [HP1 HQ1]; simpl; split.
-    - eapply is_antitone; try apply Hprec; eauto.
-    - eapply is_antitone; try apply Hprec; eauto.
+    - eapply is_antitone_anti; try apply Hprec; eauto.
+    - eapply is_antitone_anti; try apply Hprec; eauto.
   Qed.
   Next Obligation with eauto.
-    intros P Q HP HQ a Hac; simpl; split; intros [HP1 HQ1]; split; try eapply complete_monotone_is_equivalent...
+    intros P Q HP HQ a; simpl in *; intros [HP1 HQ1]. split; try eapply approx_mono...
   Qed.
   Next Obligation with eauto.
-    intros P Q HP HQ a Hac; simpl; split; intros [HP1 HQ1]; split; try eapply complete_antitone_is_equivalent...
+    intros P Q HP HQ a; simpl in *; intros [HP1 HQ1]. split; try eapply approx_anti...
+  Qed.
+  Next Obligation with eauto.
+    intros P Q HP HQ a Hac; simpl; intros [HP1 HQ1]; split; try eapply complete_monotone_is_equivalent...
+  Qed.
+  Next Obligation with eauto.
+    intros P Q HP HQ a Hac; simpl; intros [HP1 HQ1]; split; try eapply complete_antitone_is_equivalent...
   Qed.
 
   #[export] 
-  Program Instance monotonizableDisj (P Q : A -> Prop) {HP : Monotonizable P} {HQ : Monotonizable Q} : 
-    Monotonizable (fun a => P a \/ Q a) :=
+  Program Instance IncRefDisj (P Q : A -> Prop) {HP : IncRef P} {HQ : IncRef Q} : 
+    IncRef (fun a => P a \/ Q a) :=
     {
-      monotone := fun a => (monotone P a) \/ (monotone Q a) ;
-      antitone := fun a => (antitone P a) \/ (antitone Q a) ;
+      ir_mono := fun a => (ir_mono P a) \/ (ir_mono Q a) ;
+      ir_anti := fun a => (ir_anti P a) \/ (ir_anti Q a) ;
     }.
   Next Obligation.
     intros P Q HP HQ a1 a2 Hprec [HP1 | HQ1]; simpl.
-    - left; eapply is_monotone; try apply Hprec; eauto.
-    - right; eapply is_monotone; try apply Hprec; eauto.
+    - left; eapply is_monotone_mono; try apply Hprec; eauto.
+    - right; eapply is_monotone_mono; try apply Hprec; eauto.
   Qed.
   Next Obligation.
     intros P Q HP HQ a1 a2 Hprec [HP1 | HQ1]; simpl.
-    - left; eapply is_antitone; try apply Hprec; eauto.
-    - right; eapply is_antitone; try apply Hprec; eauto.
+    - left; eapply is_antitone_anti; try apply Hprec; eauto.
+    - right; eapply is_antitone_anti; try apply Hprec; eauto.
   Qed.
   Next Obligation with eauto.
-    intros P Q HP HQ a Hac; simpl; split; intros [HP1 | HQ1];
+    intros P Q HP HQ a; simpl in *; intros [HP1 | HQ1];
+      try (now left; apply approx_mono; eauto);
+      try (now right; apply approx_mono; eauto).
+  Qed.
+  Next Obligation with eauto.
+    intros P Q HP HQ a; simpl in *; intros [HP1 | HQ1];
+      try (now left; apply approx_anti; eauto);
+      try (now right; apply approx_anti; eauto).
+  Qed.
+  Next Obligation with eauto.
+    intros P Q HP HQ a Hac; simpl; intros [HP1 | HQ1];
       try (now left; apply complete_monotone_is_equivalent; eauto);
       try (now right; apply complete_monotone_is_equivalent; eauto).
   Qed.
   Next Obligation with eauto.
-    intros P Q HP HQ a Hac; simpl; split; intros [HP1 | HQ1];
+    intros P Q HP HQ a Hac; simpl; intros [HP1 | HQ1];
       try (now left; apply complete_antitone_is_equivalent; eauto);
       try (now right; apply complete_antitone_is_equivalent; eauto).
   Qed.
 
   #[export] 
-  Program Instance monotonizableArrow {P Q : A -> Prop} {HP : Monotonizable P} {HQ : Monotonizable Q} : 
-    Monotonizable (fun a => P a -> Q a) :=
+  Program Instance IncRefArrow {P Q : A -> Prop} {HP : IncRef P} {HQ : IncRef Q} : 
+    IncRef (fun a => P a -> Q a) :=
     {
-      monotone := fun a => antitone P a -> monotone Q a ;
-      antitone := fun a => monotone P a -> antitone Q a ;
+      ir_mono := fun a => ir_anti P a -> ir_mono Q a ;
+      ir_anti := fun a => ir_mono P a -> ir_anti Q a ;
     }.
   Next Obligation.
     simpl; intros P Q HP HQ a1 a2 Hprec ? Hanti.
-    eapply HQ.(is_monotone).
+    eapply HQ.(is_monotone_mono).
     - apply Hprec.
-    - apply H. eapply HP.(is_antitone); eauto.
+    - apply H. eapply HP.(is_antitone_anti); eauto.
   Qed.
   Next Obligation.
     simpl; intros P Q HP HQ a1 a2 Hprec ? Hanti.
-    eapply HQ.(is_antitone).
+    eapply HQ.(is_antitone_anti).
     - apply Hprec.
-    - apply H. eapply HP.(is_monotone); try apply Hprec; eauto.
+    - apply H. eapply HP.(is_monotone_mono); try apply Hprec; eauto.
   Qed.
   Next Obligation with eauto.
-    simpl; intros P Q HP HQ a Ha; split; intros ?.
-    - intros H1. eapply HQ.(complete_monotone_is_equivalent); eauto.
-      apply H. eapply HP.(complete_antitone_is_equivalent); eauto.
-    - intros H1. eapply HQ.(complete_monotone_is_equivalent); eauto.
-      apply H. eapply HP.(complete_antitone_is_equivalent); eauto.
+    simpl; intros P Q HP HQ a Hf Ha.
+    now eapply approx_mono, Hf, approx_anti.
   Qed.
   Next Obligation with eauto.
-    simpl; intros P Q HP HQ a Ha; split; intros ?.
-    - intros H1. eapply HQ.(complete_antitone_is_equivalent); eauto.
-      apply H. eapply HP.(complete_monotone_is_equivalent); eauto.
-    - intros H1. eapply HQ.(complete_antitone_is_equivalent); eauto.
-      apply H. eapply HP.(complete_monotone_is_equivalent); eauto.
+    simpl; intros P Q HP HQ a Hf Ha.
+    now eapply approx_anti, Hf, approx_mono.
+  Qed.
+  Next Obligation with eauto.
+    simpl; intros P Q HP HQ a Ha; intros ?.
+    - intros H1. eapply complete_monotone_is_equivalent; eauto.
+      apply H. eapply complete_antitone_is_equivalent; eauto.
+  Qed.
+  Next Obligation with eauto.
+    simpl; intros P Q HP HQ a Ha; intros ?.
+    - intros H1. eapply (complete_antitone_is_equivalent); eauto.
+      apply H. eapply (complete_monotone_is_equivalent); eauto.
   Qed.
 
-End Monotonicity.
+End IncReformulation.
 
-Arguments monotone {A HA HAC} P {Monotonizable} _. 
-Arguments antitone {A HA HAC} P {Monotonizable} _. 
+Arguments ir_mono {A HA HAC} P {IncRef} _. 
+Arguments ir_anti {A HA HAC} P {IncRef} _. 
 
-Lemma monotonizable_equiv : forall A `{Refinable A} `{Complete A} (P Q : A -> Prop) , 
-  Monotonizable P -> 
+Lemma IncRefEquiv : forall A `{Refinable A} `{Complete A} (P Q : A -> Prop) , 
+  IncRef P -> 
   (forall a, P a <-> Q a) -> 
-  Monotonizable Q.
+  IncRef Q.
 Proof.
   intros A HAR HAC P Q HPmono Hequiv.
   unshelve econstructor.
-  - exact (monotone P).
-  - exact (antitone P).
-  - eapply HPmono.(is_monotone).
-  - eapply HPmono.(is_antitone).
-  - intros a Hac; split.
-    * intros HPa. apply Hequiv. eapply HPmono.(complete_monotone_is_equivalent); eauto.
-    * intros HQa. apply Hequiv in HQa. eapply HPmono.(complete_monotone_is_equivalent); eauto.
-  - intros a Hac; split.
-    * intros HPa. apply Hequiv. eapply HPmono.(complete_antitone_is_equivalent); eauto.
-    * intros HQa. apply Hequiv in HQa. eapply HPmono.(complete_antitone_is_equivalent); eauto.
+  - exact (ir_mono P).
+  - exact (ir_anti P).
+  - eapply HPmono.(is_monotone_mono).
+  - eapply HPmono.(is_antitone_anti).
+  - intros a Ha. now eapply approx_mono, Hequiv. 
+  - intros a Ha. now eapply Hequiv, approx_anti. 
+  - intros a Hac.
+    * intros HPa. apply Hequiv. eapply (complete_monotone_is_equivalent); eauto.
+  - intros a Hac.
+    * intros HQa. apply Hequiv in HQa. eapply (complete_antitone_is_equivalent); eauto.
 Qed.
 
 #[export] 
-Instance monotonizable_eq_fun {A} `{Refinable A} `{Complete A} 
+Instance IncRefEq_fun {A} `{Refinable A} `{Complete A} 
   {B} `{Refinable B} 
   {C} `{Refinable C}
-  (g h : A -> B -> C) {Hmono : Monotonizable (fun a => forall b, g a b = h a b)} 
-  : Monotonizable (fun a => g a = h a).
+  (g h : A -> B -> C) {Hmono : IncRef (fun a => forall b, g a b = h a b)} 
+  : IncRef (fun a => g a = h a).
 Proof.
-  eapply monotonizable_equiv.
+  eapply IncRefEquiv.
   - apply Hmono.
   - intros a; split.
     * intros Heq. apply functional_extensionality; eauto.
